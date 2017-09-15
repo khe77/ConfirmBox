e654a25feac42519335402facd338558982cfe65
khe77 <khe7758@gmail.com>
2017-09-15 15:22:16 +0900

    List, View, PushSetting 액티비티 추가

---
+++ app/src/main/java/org/khe77/confirmbox/PushSettingActivity.java
@@ -0,0 +1,13 @@
+package org.khe77.confirmbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PushSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_push_setting);
+    }
+}
